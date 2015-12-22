a943ac256789bd2ffe0eef075b567abfb19313c0
Distra Vantari <distravantari@gmail.com>
2015-12-22 19:29:46 +0700

    version 1.6
    update setting

--- model/APIarthentic.js
+++ model/APIarthentic.js
@@ -94,6 +94,36 @@ APIarthentic.prototype.handleRoutes = function(router,connection,md5)
           });
       });
 
+      //get setting
+      router.get("/setting",function(req,res){
+        connection.query("SELECT * FROM `setting`",function(err,rows){
+          if (err) {
+            res.json({"message":"err.. "+err});
+          }else{
+            res.json({"message":rows});
+          }
+        })
+      })
+
+      //ubah settingan
+      router.post("/setting",function(req,res){
+        var tax = req.body.tax;
+        var services = req.body.services;
+
+        var query = "UPDATE `setting` SET `tax`= ?,`services`= ?";
+        var table = [tax,services];
+        query = mysql.format(query,table);
+        connection.query(query,function(err,rows){
+          if (err) {
+            res.json({"message":"err.. "+err});
+          }else{
+            res.json({"message":"success"});
+          }
+        })
+      })
+
+
+
       router.post("/showMenuById",function(req,res){
         var id = req.body.id;
         // var id = req.body.id;
