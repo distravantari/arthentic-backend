06a9843f9045e8ad2eb9c9864fd3a60e3dd622b0
Distra Vantari <distravantari@gmail.com>
2015-12-27 08:28:02 +0700

    version 2.6.8
    history bug fixes

--- model/history.js
+++ model/history.js
@@ -8,8 +8,6 @@ history.prototype.handleRoutes = function(router,connection,md5)
 {
   //insert history (id, namaUser, perubahan, row)
   router.post("/insertHistory",function(req,res){
-    //request id
-    var id = req.body.id;
     //request nama
     var namaUser = req.body.namaUser;
     //request perubahan
@@ -17,45 +15,24 @@ history.prototype.handleRoutes = function(router,connection,md5)
     //request row (integer) mau rubah row 1/2/3/4/5/6/7/..../99
     var row = req.body.row;
     //request date (format dd-mm-yyyy, ex. 12-12-2015)
-    var date = req.body.date;
+    var status = req.body.status;
 
-     //query checking id jika sudah ada yang sama
-     var query1 = "select id from history where id = ?";
-     var table1 = [id];
-     query1 = mysql.format(query1,table1);
+    //query insert
+    var query = "insert into `history` (nama,perubahan,row,status) VALUES (?,?,?,?)";
+    var table = [namaUser,perubahan,row,status];
+    query = mysql.format(query, table);
 
-     connection.query(query1,function(err,temp)
-     {
-        //Jika id menu belum ada di database
-        if(temp.length == 0 )
-        {
-            //query insert
-            var query = "insert into `history` (id,nama,perubahan,row,date) VALUES (?,?,?,?,?)";
-            var table = [id,namaUser,perubahan,row,date];
-            query = mysql.format(query, table);
+    connection.query(query,function(err,temp){
+      if(err)
+      {
+          res.json({"message":err});
+      }
+      else
+      {
 
-            connection.query(query,function(err,temp){
-              if(err)
-              {
-                  res.json({"message":err});
-              }
-              else
-              {
-
-                  res.json({"message":"input history berhasil" });
-              }
-            });
-        }
-        else if(err)
-        {
-          res.json({"message":err})
-        }
-        //Jika barang sudah ada di database
-        else
-        {
-          res.json({"message":"id  "+id+ " sudah ada di database!"});
-        }
-     });
+          res.json({"message":"input history berhasil" });
+      }
+    });
  });
   //--insert history end..
 
